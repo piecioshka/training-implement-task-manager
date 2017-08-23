b3156d4351664d4071be771938fc2e20b9f6c20e
Robert Obreczarek <robert@obreczarek.com>
2017-08-23 20:22:58 +0200

    Converted to lowercase and minor text changes

--- test/unit/specs/test.task-manager.js
+++ test/unit/specs/test.task-manager.js
@@ -68,13 +68,13 @@ QUnit.module('TaskManager', function () {
         hooks.beforeEach(function () {
             this.fake = {
                 bar: function () {
-                    this.step('The function from the first task has executed');
+                    this.step('the function from the first task has executed');
                 },
                 baz: function () {
-                    this.step('The function from the second task has executed');
+                    this.step('the function from the second task has executed');
                 },
                 qux: function () {
-                    this.step('The function from the third task has executed');
+                    this.step('the function from the third task has executed');
                 }
             };
 
@@ -96,32 +96,32 @@ QUnit.module('TaskManager', function () {
             };
             let mockContext = this;
 
-            assert.strictEqual(queue.length, 0, 'The initial list of task is empty');
+            assert.strictEqual(queue.length, 0, 'the initial list of task is empty');
 
             this.tasks.addTask(mockName, mockFunction, mockContext);
 
-            assert.strictEqual(queue.length, 1, 'After adding a task to the empty list, the list should contain one task');
+            assert.strictEqual(queue.length, 1, 'after adding a task to the empty list, the list should contain one task');
 
             assert.strictEqual(typeof queue[0].name, 'string', 'First element should have a string property called "name"');
-            assert.strictEqual(queue[0].name, mockName, 'Property "name" should be the first parameter of the "addTask" function');
+            assert.strictEqual(queue[0].name, mockName, 'property "name" should be the first parameter of the "addTask" function');
 
-            assert.strictEqual(typeof queue[0].callback, 'function', 'The first element should have a function property named "callback"');
-            assert.strictEqual(queue[0].callback, mockFunction, 'The property "callback" should be the second parameter of the "addTask" function');
+            assert.strictEqual(typeof queue[0].callback, 'function', 'the first element should have a function property named "callback"');
+            assert.strictEqual(queue[0].callback, mockFunction, 'the property "callback" should be the second parameter of the "addTask" function');
 
             assert.strictEqual(typeof queue[0].context, 'object', 'First element should have an object property named "context"');
-            assert.strictEqual(queue[0].context, mockContext, 'The property "context" should be the third parameter of the "addTask" function');
+            assert.strictEqual(queue[0].context, mockContext, 'the property "context" should be the third parameter of the "addTask" function');
         });
 
-        QUnit.test('The callback for the first task should be executed when the "run" function is called', function (assert) {
+        QUnit.test('the callback for the first task should be executed when the "run" function is called', function (assert) {
             this.tasks.addTask('task 1', this.fake.bar.bind(assert));
             this.tasks.run();
 
             assert.verifySteps([
-                'Function from first task executed'
+                'the function from the first task has executed'
             ]);
         });
 
-        QUnit.test('Each task item should have its callback executed via the "run" function', function (assert) {
+        QUnit.test('each task item should have its callback executed via the "run" function', function (assert) {
             this.tasks.addTask('task 2', this.fake.bar.bind(assert));
             this.tasks.addTask('task 3', this.fake.baz.bind(assert));
             this.tasks.addTask('task 4', this.fake.qux.bind(assert));
@@ -129,16 +129,16 @@ QUnit.module('TaskManager', function () {
             this.tasks.run();
 
             assert.verifySteps([
-                'Function from first task executed',
-                'Function from second task executed',
-                'Function from third task executed'
+                'the function from the first task executed',
+                'the function from the second task executed',
+                'the function from the third task executed'
             ]);
         });
 
         QUnit.test('should run with the context that was passed', function (assert) {
             let fake = this.fake;
             this.tasks.addTask('task 5', function () {
-                assert.strictEqual(this, fake, 'The context of the executed callback should be equal to the object which was passed through the "add" function');
+                assert.strictEqual(this, fake, 'the context of the executed callback should be equal to the object which was passed through the "add" function');
             }, fake);
 
             this.tasks.run();
